0b38f98edc94dfefc4ddf80c6952ddfed86d9d11
bernaotieno <bernadokumu1@gmail.com>
2024-11-07 14:32:38 +0300

    back tyo my main files

--- api/config.js
+++
@@ -1,3 +0,0 @@
-const requiredEnvVars = ['API_KEY', 'SECRET_KEY'];
-
-export default requiredEnvVars;
--- middleware/auth.js
+++
@@ -1,9 +0,0 @@
-export const verifyApiKey = (req, res, next) => {
-    const providedKey = req.headers['x-api-key'];
-    const apiKey = process.env.API_KEY;
-
-    if (!providedKey || providedKey !== apiKey) {
-        return res.status(401).json({ error: 'Unauthorized: Invalid API key' });
-    }
-    next();
-};
